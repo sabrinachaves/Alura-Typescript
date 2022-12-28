5618f27b5a5d4e1e29d6b1da7d4fe7d6d9aed4dd
sabrinachaves <sabrinach08@gmail.com>
2022-12-28 11:54:42 -0300

    Adicionando mensagem de sucesso e utilizando herança

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,11 +1,8 @@
 import { NegociacaoController } from "../controllers/negociacao-controller";
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView {
-  private elemento: HTMLElement;
-  constructor(seletor: string) {
-    this.elemento = document.querySelector(seletor);
-  }
+export class NegociacoesView extends View{
 
   template(model: Negociacoes): string {
     return `
